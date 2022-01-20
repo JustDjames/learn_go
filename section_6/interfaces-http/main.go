1c2ab93ae78eeae2e552e60c24b6e9e7b6b6e71e
JustDjames <djames7776@gmail.com>
2022-01-20 17:19:47 +0000

    completed interface-http section

---
+++ section_6/interfaces-http/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	// the make() function is used to make a slice, map or chan (channel) of a specified length.
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+// This is method allows the logWriter structure to be included in the Writer type. This allows it to be used with the io.Copy method above
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
